4f938336f0cdc709b959d6eab7968554fce60e70
hassan-jamshaid10 <hjamshaid81@gmail.com>
2024-09-28 02:13:30 +0500

    Made Responsive Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './Components/Header'
-
+import LandingPage from './Components/LandingPage'
 const App = () => {
   return (
     <main className='overflow-hidden'>
     <Header/>
+    <LandingPage/>
     </main>
   )
 }
