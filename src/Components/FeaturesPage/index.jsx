93e4f930721199a8cd75b2003dc9003b052e706b
hassan-jamshaid10 <hjamshaid81@gmail.com>
2024-10-02 02:05:06 +0500

    Made Features and details section | Responsive Layout

--- src/Components/FeaturesPage/index.jsx
+++ src/Components/FeaturesPage/index.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { features } from '../../Constants';
 import { Element } from 'react-scroll';
+import Button from '../../ReusableComponents/Button/index';
+import DetailsPage from '../Details/index';
 
 const FeaturesPage = () => {
   return (
   <section>
    <Element name="featuresPage">
     <div className='container'>
-        <div className='relative flex'>
-         {features.map((feature)=>(
-            <div key={feature.id}>
-            {feature.title}
-            </div>
+        <div className='relative flex md:flex-wrap flex-nowrap border-2 
+        border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7
+        max-md:border-none max-md:rounded-none max-md:gap-3 '>
+
+         {features.map(({id,icon,caption,title,text,button})=>(
+            <div key={id} className='relative z-2 md:px-10 px-5 
+            md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl
+            max-md:flex-320'>
+             <div className='w-full flex justify-start items-start'>
+                <div className='-ml-3 mb-12 flex items-center justify center flex-col'>
+                    <div className='w-0.5 h-16 bg-s3'/>
+              <img src={icon} className='size-28 object-container' alt={title}/>
+               </div>
+             </div>
+             <p className="caption mb-5 max-md:mb-6">{caption}</p>
+                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                  {title}
+                </h2>
+                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                <Button icon={button.icon}>{button.title}</Button>
+              </div>
          ))}
+         <DetailsPage/>
         </div>
     </div>
    </Element>
