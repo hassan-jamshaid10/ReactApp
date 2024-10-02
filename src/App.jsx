a6eeef38943022c998abf3746eebbca41de1b8e8
hassan-jamshaid10 <hjamshaid81@gmail.com>
2024-10-02 23:18:40 +0500

    Made Faqs Page | Responsive Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import Header from './Components/Header'
-import LandingPage from './Components/LandingPage'
-import FeaturesPage from './Components/FeaturesPage'
-import Pricing from './Components/Pricing/index'
+import React from 'react';
+import Header from './Components/Header';
+import LandingPage from './Components/LandingPage';
+import FeaturesPage from './Components/FeaturesPage';
+import Pricing from './Components/Pricing/index';
+import Faq from './Components/Faq/index';
 const App = () => {
   return (
     <main className='overflow-hidden'>
@@ -11,6 +12,7 @@ const App = () => {
     <LandingPage/>
     <FeaturesPage/>
     <Pricing/>
+    <Faq/>
     </main>
   )
 }
