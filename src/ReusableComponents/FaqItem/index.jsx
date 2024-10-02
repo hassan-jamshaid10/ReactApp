e9dafeab9249bd8ababa30166bc1e0be434dcec7
hassan-jamshaid10 <hjamshaid81@gmail.com>
2024-10-02 23:31:11 +0500

    Made Faqs  | Responsive Layout

--- src/ReusableComponents/FaqItem/index.jsx
+++ src/ReusableComponents/FaqItem/index.jsx
@@ -1,8 +1,7 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
-
+import { Collapse } from "react-collapse";
+//--
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
 
@@ -41,11 +40,9 @@ const FaqItem = ({ item, index }) => {
         </div>
       </div>
 
-      <SlideDown>
-        {activeId === item.id && (
-          <div className="body-3 px-7 py-3.5">{item.answer}</div>
-        )}
-      </SlideDown>
+      <Collapse isOpened={activeId === item.id}>
+        <div className="body-3 px-7 py-3.5">{item.answer}</div>
+      </Collapse>
 
       <div
         className={clsx(
